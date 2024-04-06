e478bf46f02c817d4da6ec9f714b7f8ecb2d1db2
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-04-06 10:05:27 +0530

    feat:db connection done using mongoose

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -1,7 +1,9 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT || 3000
-}
+  PORT: process.env.PORT || 3000,
+  MONGODB_CONNECTION_URL: process.env.MONGODB_CONNECTION_URL,
+  NODE_ENV:process.env.NODE_ENV
+};
